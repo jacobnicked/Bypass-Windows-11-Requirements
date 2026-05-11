eea8003ca08d637f9d9d407f679dd75fbb33bc4c
Jacob Nicked <git@jacobnicked.com>
2026-05-11 23:34:59 +0200

    Enhance Windows 11 bypass script with more checks
    Updated the script to include additional registry keys for bypassing Windows 11 requirements.

--- BypassWindows11Requirements.bat
+++ BypassWindows11Requirements.bat
@@ -1,9 +1,16 @@
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig\ /v BypassTPMCheck /t REG_DWORD /d 1 /f
+@echo off
+:: Windows 11 Requirements Bypass Script
+:: Adds registry keys to skip TPM 2.0, Secure Boot, and CPU checks
 
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig\ /v BypassRAMCheck /t REG_DWORD /d 1 /f
+echo Applying Windows 11 bypass registry keys...
 
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig\ /v BypassSecureBootCheck /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v AllowUpgradesWithUnsupportedTPMOrCPU /t REG_DWORD /d 1 /f
 
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig\ /v BypassCPUCheck /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v BypassTPMCheck /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v BypassSecureBootCheck /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v BypassRAMCheck /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v BypassStorageCheck /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v BypassCPUCheck /t REG_DWORD /d 1 /f
 
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup\ /v AllowUpgradesWithUnsupportedTPMOrCPU /t REG_DWORD /d 1 /f
+echo Done! You can now run the Windows 11 installer.
+pause
